363db0fdf3f24a1119607e61ec2cc4242dbdbdf4
Yogesh_Reddy <yogeshwarreddyp@gmail.com>
2023-02-05 03:04:10 +0530

    final commit switch case while pending

---
+++ learnSwitchcase.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo choose the option below
+echo 1=date
+echo 2=current working direc
+echo 3=presentworking directory
+
+read choice
+
+case $choice in
+    1) date;;
+    2) ls -ltr;;
+    3) pwd;;
+    4) cat learnS*.sh;;
+    5) cat lear_if*.sh;;
+    *) echo get lost;;
+    
+
+    
+esac
